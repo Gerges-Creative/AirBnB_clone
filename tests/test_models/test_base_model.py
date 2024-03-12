b84567709a6664474a2765ec3c14c11de4035015
Gerges-Creative <gerges.zechariah@gmail.com>
2024-03-12 07:49:00 -0700

    Test str

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,15 +4,23 @@ import unittest
 
 class TestBaseModel(unittest.TestCase):
 
-    def test_save(self):
-        model = BaseModel()
-        initial_updated_at = model.updated_at
-        model.save()
-        self.assertNotEqual(initial_updated_at, model.updated_at)
+    @classmethod
+    def setUpClass(cls):
+        cls.model = BaseModel()
+        cls.model.name = "Gerges"
+        cls.model.my_number = 24
 
-    def test_str(self):
-        model = BaseModel()
-        self.assertTrue(model.__str__)
+
+    def test_save(self):
+        initial_updated_at = self.model.updated_at
+        self.model.save()
+        self.assertNotEqual(initial_updated_at, self.model.updated_at)
+
+    def test_to_dict(self):
+        model1_dict = self.model.to_dict()
+        self.assertEqual(self.model.__class__.__name__, "BaseModel")
+        self.assertIsInstance(model1_dict["created_at"], str)
+        self.assertIsInstance(model1_dict["updated_at"], str)
 
 if __name__ == "__main__":
     unittest.main()

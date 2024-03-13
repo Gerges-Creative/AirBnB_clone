3c10d1ee8fb105c80318c2a934926d65ca8c2acc
salmaa2005 <salmaamostafa2005@gmail.com>
2024-03-13 05:06:13 +0200

    fix __init.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,84 +1,56 @@
 #!/usr/bin/python3
-""" This script defines the basic attributes and methods for the entire project
+"""This script is the base model"""
 
-The the datetime module is used to store the time at creation and updating.
-
-The UUID module is used to generate uniqe ids for every instance created
-for this object.
-"""
-import datetime
 import uuid
+from datetime import datetime
+from models import storage
 
 
 class BaseModel:
-    """
-    This class is the main class used to define all common attributes and
-    methods for other classes
-    """
 
-    # This cls attr is for the setattr to check if attr assignment has occured
-    _is_changed = False
+    """Class from which all other classes will inherit"""
 
     def __init__(self, *args, **kwargs):
-        """
-        This the constructor method doesn't recieve any parameters but defines
-        all the used ones throughout the project
+        """Initializes instance attributes
+
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
         """
 
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                # setattr is a built-in function that sets the value of an
-                # attribute on an object.
-                setattr(self, key, value)
-                self.created_at = datetime.datetime.fromisoformat(
-                    kwargs["created_at"])
-                self.updated_at = datetime.datetime.fromisoformat(
-                    kwargs["updated_at"])
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
         else:
-            self.my_number = None
-            self.name = None
-            self.created_at = datetime.datetime.now()
-            self.updated_at = self.created_at
             self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
-        """
-        The __str__ method is used to return a string of basic info
-        """
-        if BaseModel._is_changed is True:
-            self.updated_at = datetime.datetime.now()
-            BaseModel._is_changed = False
-        return ("[BaseModel] ({}) {}".format(self.id, self.__dict__))
+        """Returns official string representation"""
 
-    # For now its just used as check point to updated time
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
+
     def save(self):
-        """
-        The save method is used to update time for the attribute updated_at
-        """
-        self.updated_at = datetime.datetime.now()
+        """updates the public instance attribute updated_at"""
 
-    def __setattr__(self, key, value):
-        """
-        The __setattr__ method is used to check if any assignments have occured
-        in the object instance
-        """
-        if key != '_is_changed':
-            BaseModel._is_changed is True
-        super(BaseModel, self).__setattr__(key, value)
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        """
-        The to_dict method is used to modify the format of created_at and
-        updated_atand add a new attribute called __class__ to __dict__
-        """
-        if BaseModel._is_changed is True:
-            self.updated_at = datetime.datetime.now()
-            BaseModel._is_changed = False
+        """returns a dictionary containing all keys/values of __dict__"""
 
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
-        # There is another way by making a copy of BaseModel first then modify
-        self.__dict__["__class__"] = "BaseModel"
-        return self.__dict__
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
